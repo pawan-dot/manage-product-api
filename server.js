91704c1b94596e305bd2b22982a1aa3cb4ffbbfb
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2021-10-06 11:19:32 +0530

    node modules deleted

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 //db connection
-require('./db/conn');
+require("./db/conn");
 
 app.use(express.json());
 
 //calling routes function
 require("./Routes/route")(app);
 app.listen(PORT, () => {
-    console.log(`server is listening on ${PORT}`);
-  });
+  console.log(`server is listening on ${PORT}`);
+});
